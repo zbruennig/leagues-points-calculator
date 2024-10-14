2b4200ca3d2930cfc67ed1d2c7cbc6c522881825
Zach <zach.bruennig@gmail.com>
2024-10-14 00:07:20 -0500

    split out varlamore to comment out easier

--- helper.py
+++ helper.py
@@ -5,7 +5,9 @@ from classes import Regions
 
 def get_region_combos() -> List[Regions]:
     combinations = []
-    regions = ['v', 'z', 't', 'k', 'a', 'f', 'w', 'd', 'm']
+    regions = [
+        'v',
+        'z', 't', 'k', 'a', 'f', 'w', 'd', 'm']
     length = len(regions)
     for i in range(0, length):
         for j in range(i+1, length):
