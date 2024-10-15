1a75a8edde6c782f4f8b3a84dd0e24a3c7c142a1
Zach <zach.bruennig@gmail.com>
2024-10-14 22:02:54 -0500

    Remove empty file

--- boss_list.py
+++ boss_list.py
@@ -1,5 +1,11 @@
 from classes import Boss
 
+# Going to assume that CAs which are region locked are autoed, like they were changed in the middle of leagues 4
+# Applies to ones needing specific lunar/ancient/arceuus spells, items from other regions, etc.
+# Basically, if the boss is available with your regions then its CAs are all doable.
+
+# DT2 bosses require multiple regions (as far as we know), so we need to make sure all are satisfied
+
 # TODO for new league uncomment the bosses that didn't exist for TBL / no CAs
 
 bosses = [
